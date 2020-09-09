cfa46371467bbe01dc98e5d121258076cbb96dba
Andre Luiz <andreluizkanaan222@gamil.com>
2020-09-08 21:34:24 -0300

    Increases

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,13 +2,22 @@ import React, { useEffect, useState } from "react";
 import api from "../../services/axios";
 import requests from "../../services/requests";
 
-import { Container, BannerContent, Title, BannerButtons } from "./styles";
+import {
+  Container,
+  BannerContent,
+  Title,
+  BannerButtons,
+  ButtonPlay,
+  ButtonMyList,
+  Description,
+} from "./styles";
 
 interface Movie {
   name?: string;
   title?: string;
   backdrop_path?: string;
   original_name?: string;
+  overview?: string;
 }
 
 const Banner: React.FC = () => {
@@ -28,31 +37,17 @@ const Banner: React.FC = () => {
     fetchData();
   }, []);
 
+  function truncate(str: string | undefined, n: number) {
+    if (typeof str === "string") {
+      if (str.length > n) {
+        return str.substr(0, n - 1) + "...";
+      }
+    } else {
+      return "";
+    }
+  }
+
   return (
-    // <Container>
-    //   {movies.map((movie: Movie) => {
-    //     return (
-    //       <div
-    //         style={{
-    //           backgroundSize: "cover",
-    //           backgroundImage: `url(
-    //           "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
-    //         )`,
-    //           backgroundPosition: "center center",
-    //         }}
-    //       >
-    //         <BannerContent>
-    //           <Title>
-    //             {movie?.title || movie?.name || movie?.original_name}
-    //           </Title>
-
-    //           <BannerButtons></BannerButtons>
-    //         </BannerContent>
-    //       </div>
-    //     );
-    //   })}
-    // </Container>
-
     <Container
       style={{
         backgroundSize: "cover",
@@ -65,7 +60,12 @@ const Banner: React.FC = () => {
       <BannerContent>
         <Title>{movie?.title || movie?.name || movie?.original_name}</Title>
 
-        <BannerButtons></BannerButtons>
+        <BannerButtons>
+          <ButtonPlay>Iniciar</ButtonPlay>
+          <ButtonMyList>Minha Lista</ButtonMyList>
+        </BannerButtons>
+
+        <Description>{truncate(movie?.overview, 250)}</Description>
       </BannerContent>
     </Container>
   );
--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,9 +1,79 @@
 import styled from "styled-components";
 
-export const Container = styled.header``;
+export const Container = styled.header`
+  color: var(--color-title-in-primary);
+  object-fit: contain;
+  height: 44.8rem;
+`;
 
-export const BannerContent = styled.div``;
+export const BannerContent = styled.div`
+  margin-left: 3rem;
+  padding-top: 14rem;
+  height: 19rem;
+  /* display: flex;
+  flex-direction: column; */
+`;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+  font-size: 5rem;
+  font-weight: 800;
+  padding-bottom: 1.8rem;
+`;
 
-export const BannerButtons = styled.div``;
+export const BannerButtons = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const ButtonPlay = styled.button`
+  cursor: pointer;
+  color: var(--color-title-in-primary);
+  height: 4rem;
+  outline: none;
+  border: none;
+  font-weight: 700;
+  border-radius: 0.2vw;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  margin-right: 1rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  background-color: rgba(51, 51, 51, 0.5);
+
+  &:hover {
+    color: var(--color-background);
+    background-color: #e6e6e6;
+    transition: all 0.2s;
+  }
+`;
+
+export const ButtonMyList = styled.button`
+  cursor: pointer;
+  color: var(--color-title-in-primary);
+  height: 4rem;
+  outline: none;
+  border: none;
+  font-weight: 700;
+  border-radius: 0.2vw;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  margin-right: 1rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  background-color: rgba(51, 51, 51, 0.5);
+
+  &:hover {
+    color: var(--color-background);
+    background-color: #e6e6e6;
+    transition: all 0.2s;
+  }
+`;
+
+export const Description = styled.h1`
+  width: 45rem;
+  line-height: 2.3rem;
+  padding-top: 1rem;
+  font-size: 1.8rem;
+  max-width: 36rem;
+  height: 8rem;
+`;
